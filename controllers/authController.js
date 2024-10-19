61d5b5eb9ffb56ed9e00c6266bf627c15d7452ef
Yerbol <yerbol_kosbayev@list.ru>
2024-10-19 13:40:07 +0500

    fix(user): fixed user management
    -added user data in login response

--- controllers/authController.js
+++ controllers/authController.js
@@ -48,7 +48,14 @@ export const login = async (req, res) => {
 
     jwt.sign(payload, process.env.JWT_SECRET, { expiresIn: 3600 }, (err, token) => {
       if (err) throw err;
-      res.json({ token });
+      res.json({
+        token,
+        user: {
+          id: user.id,
+          username: user.username,
+          role: user.role,
+        },
+      });
     });
   } catch (err) {
     console.error(err.message);
